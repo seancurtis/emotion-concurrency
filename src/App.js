6af0a4ab2f3ed0f48078f54df74c512f430831b2
Matt Charlton <mcharlton@atlassian.com>
2021-04-27 16:05:06 +1000

    repro found

--- src/App.js
+++ src/App.js
@@ -1,40 +1,36 @@
-import React, { useState } from "react";
-
-import Button from "@atlaskit/button";
+import React, { useEffect, useState } from "react";
 
 import styled from "styled-components";
 
-import Select from "react-select/async";
-
 import Emotion10 from "./emo10";
-import Emotion11 from "./emo11";
 
 const Div = styled.div`
   background: #ffdd11;
   min-height: 200px;
 `;
 
-const options = [
-  { value: "chocolate", label: "Chocolate" },
-  { value: "strawberry", label: "Strawberry" },
-  { value: "vanilla", label: "Vanilla" },
-];
+async function loadEmotion11() {
+  return await import("./emo11");
+}
 
 function App() {
-  const [loading, setLoading] = useState(false);
+  const [Emotion11, setComp] = useState('p');
+
+  useEffect(
+    () =>
+      setTimeout(
+        () => loadEmotion11().then((mod) => setComp(mod.default)),
+        1000
+      ),
+    []
+  );
+
+  console.log(Emotion11);
   return (
     <>
       <Div>
         <Emotion10 />
-        <Emotion11 />
-        <Select
-          isLoading={loading}
-          defaultOptions={options}
-          components={{ LoadingIndicator: Emotion10 }}
-        />
-        <Button appearance="primary" onClick={() => setLoading(!loading)}>
-          Click me
-        </Button>
+        {Emotion11}
         test
       </Div>
     </>
--- src/emo11.js
+++ src/emo11.js
@@ -2,21 +2,31 @@
 /** @jsx jsx */
 import { jsx, css } from "@emotion/react";
 
+import Select from "react-select";
+
+const options = [
+  { value: "chocolate", label: "Chocolate" },
+  { value: "strawberry", label: "Strawberry" },
+  { value: "vanilla", label: "Vanilla" },
+];
+
 const styles = css`
   background: green;
   border-radius: 3px;
   padding: 0.5em;
   margin: 10px;
+  color: white;
 `;
 
-console.log('styles',styles);
+console.log("styles", styles);
 
-const E11 = () => (
-  <div
-    css={styles}
-  >
-    Emotion 11 component
-  </div>
-);
+const E11 = () => {
+  return (
+    <div css={styles}>
+      Emotion 11 component
+      <Select defaultOptions={options} />
+    </div>
+  );
+};
 
 export default E11;
